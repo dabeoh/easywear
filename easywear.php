7eb5b2fca85d3e60f9fc9ce5c95a07a9b13daff3
Hugo Mouden <hugo.mouden@gmail.com>
2021-03-30 16:57:53 +0000

    edit css

--- easywear.php
+++ easywear.php
@@ -1,22 +1,28 @@
 <html>
+
   <head>
     <meta charset="utf-8">
     <title>EasyWear</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open Sans">
     <link rel="stylesheet" href="style.css">
   </head>
+
   <body>
-  <h1 id="logo"><span id="easy">Easy</span><span id="wear">Wear</span></h1>
-  <h3> <i>Toujours plus classe, toujours plus beau ! Même quand il fait pas beau !</i></h3>
- <form method="POST" action="dress.php">
-    <input type="submit" value="M'habiller"/>
-  </form>
- <form method="POST" action="liste.php">
-    <input type="submit" value="Ma garde-robe"/>
-  </form>
-<form method="POST" action="ajoutVetement.php">
-    <input type="submit" value="Ajouter un vetement"/>
-</form>
-  </form>
- </body>
+    <div class="phone">
+      <h1 id="logo"><span id="easy">Easy</span><span id="wear">Wear</span></h1>
+      <h3> <i>Toujours plus classe, toujours plus beau ! Même quand il fait pas beau !</i></h3>
+
+      <form method="POST" action="dress.php">
+        <input type="submit" value="M'habiller"/>
+      </form>
+
+      <form method="POST" action="liste.php">
+        <input type="submit" value="Ma garde-robe"/>
+      </form>
+
+      <form method="POST" action="ajoutVetement.php">
+        <input type="submit" value="Ajouter un vetement"/>
+      </form>
+    </div>
+  </body>
 </html>
