72faadca40435372b5369fff8f4ff546ab6745fb
Hugo Mouden <hugo.mouden@gmail.com>
2021-03-30 17:50:10 +0000

    test centrage

--- easywear.php
+++ easywear.php
@@ -8,21 +8,25 @@
   </head>
 
   <body>
-    <div class="phone">
-      <h1 id="logo"><span id="easy">Easy</span><span id="wear">Wear</span></h1>
-      <h3> <i>Toujours plus classe, toujours plus beau ! Même quand il fait pas beau !</i></h3>
+    <div class="outer">
+      <div class="inner">
+        <div class="phone">
+          <h1 id="logo"><span id="easy">Easy</span><span id="wear">Wear</span></h1>
+          <h3> <i>Toujours plus classe, toujours plus beau ! Même quand il fait pas beau !</i></h3>
 
-      <form method="POST" action="dress.php">
-        <input type="submit" value="M'habiller"/>
-      </form>
+          <form method="POST" action="dress.php">
+            <input type="submit" value="M'habiller"/>
+          </form>
 
-      <form method="POST" action="liste.php">
-        <input type="submit" value="Ma garde-robe"/>
-      </form>
+          <form method="POST" action="liste.php">
+            <input type="submit" value="Ma garde-robe"/>
+          </form>
 
-      <form method="POST" action="ajoutVetement.php">
-        <input type="submit" value="Ajouter un vetement"/>
-      </form>
+          <form method="POST" action="ajoutVetement.php">
+            <input type="submit" value="Ajouter un vetement"/>
+          </form>
+        </div>
+      </div>
     </div>
   </body>
 </html>
